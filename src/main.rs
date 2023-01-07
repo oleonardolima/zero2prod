ccb7611acbedd24580b8638af31e699315b3cd3c
Leonardo L <oleonardolima@users.noreply.github.com>
2023-01-07 12:21:21 -0300

    ci: add initial actions setup (#1)
    * ci: add initial actions setup
    
    - add testing step
    - add formatting step
    - add clippy step
    - add coverage step
    
    * style: apply `cargo fmt` changes

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,7 @@ async fn health_check(_req: HttpRequest) -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-            App::new().route("/health_check", web::get().to(health_check))
-        })
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
         .run()
         .await
