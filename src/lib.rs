a5b6fe9846a71cf6ef45857355407a081f5450e5
Leonardo Lima <oleonardolima@users.noreply.github.com>
2023-01-16 23:33:19 -0300

    refactor: bind to random port and use TcpListener
    - use TcpListener to bind to random port and pass it as listener to
      HttpServer
    - update integration test to bind to random port and use it from
      SocketAddr

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
+use std::net::TcpListener;
+
 use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(lst: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
+        .listen(lst)?
         .run();
     Ok(server)
 }
