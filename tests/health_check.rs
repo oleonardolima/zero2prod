3aaa98be2babe94ce1ac779026742098208b3121
Leonardo Lima <oleonardolima@users.noreply.github.com>
2023-01-17 09:37:34 -0300

    test: add new tests for both valid/invalid data

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,15 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, vec};
+
+// launch our application in the background
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address.");
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
@@ -20,13 +31,59 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-// launch our application in the background
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
-    let port = listener.local_addr().unwrap().port();
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
 
-    let server = zero2prod::run(listener).expect("Failed to bind address.");
-    let _ = tokio::spawn(server);
+    // act
+    let body = "name=aliocha%20karamazov&email=aliocha_karamazov%40email.com";
+    let response = client
+        .post(format!("{}/subscriptions", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute the request.");
 
-    format!("http://127.0.0.1:{}", port)
+    // assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // this is an example of table driven tests (https://github.com/golang/go/wiki/TableDrivenTests)
+    let test_cases = vec![
+        ("name=fyodor%karamazov", "missing the email value"),
+        (
+            "email=dimitri_karamazov@email.com",
+            "missing the name value",
+        ),
+        ("smerdiakov", "missing both name and email values"),
+    ];
+
+    // act
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute the request.");
+
+        // assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // aditional customized error message on test failure only (https://doc.rust-lang.org/std/macro.assert.html#custom-messages)
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
