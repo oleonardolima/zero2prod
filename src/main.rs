313a2f92fcf827d7adf13e9a68f0a6f02fc30cf3
Leonardo Lima <oleonardolima@users.noreply.github.com>
2023-02-10 08:35:23 -0300

    feat: use tracing-log to redirect to subscriber

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,23 @@ use std::net::TcpListener;
 use sqlx::PgPool;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, EnvFilter, Registry};
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // redirects all third-party log's events to tracing subscriber
+    LogTracer::init().expect("Failed to set LogTracer logger");
+
     // fallback to info-level if the RUST_LOG environment variable is not set
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
     let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout); // outputs to the stdout
 
     let subscriber = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
         .with(formatting_layer);
-
     set_global_default(subscriber).expect("Failed to set up tracing subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
