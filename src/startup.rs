77e590d048f43ccc799deca70a3a6c8f82d27235
Leonardo Lima <oleonardolima@users.noreply.github.com>
2023-02-11 16:51:28 -0300

    style: fix formatting and lint

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,8 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
-use tracing_actix_web::TracingLogger;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(lst: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // wraps the db_pool in a smart pointer (Arc)
