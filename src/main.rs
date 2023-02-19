43009587d6be4053879bcd42de1366afa3802e16
Leonardo Lima <oleonardolima@users.noreply.github.com>
2023-02-19 16:45:45 -0300

    refactor: split configuration into environments
    - use a base configuration file, and specific files for local and
      production environment settings
    - update Dockerfile to use production settings
    - update configuration.rs to handle the environments hierarchy

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use zero2prod::{
     configuration::get_configuration,
     startup::run,
@@ -14,13 +14,16 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres.");
 
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))
-        .expect("Failed to bind random port.");
+    let listener = TcpListener::bind(format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    ))
+    .expect("Failed to bind random port.");
 
     run(listener, connection_pool)?.await
 }
