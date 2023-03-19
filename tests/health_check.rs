f7efce3673711b674b0f61128bd8527381658fb8
Leonardo L <oleonardolima@users.noreply.github.com>
2023-03-19 11:59:19 -0300

    feat(deployment): add digital-ocean deployment config (#9)
    * wip: add initial spec to digitalocean deployment
    
    * feat+refactor: add db to spec.yaml and configuration.rs
    
    - add new settings to spawn a database on digital ocean, at spec.yaml.
    - add new `envs` settings to spec.yaml in order to use the exposed
      environment variables at runtime.
    - refactor configuration.rs to override settings from environment
      variables, if necessary.
    - refactor `DatabaseSettings` methods to use PgConnectOptions instead of
      connetion string.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,5 @@
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
+
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::{net::TcpListener, vec};
 use uuid::Uuid;
@@ -51,17 +51,16 @@ async fn spawn_app() -> TestApp {
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // create database
-    let mut connection =
-        PgConnection::connect(config.connection_string_without_db().expose_secret())
-            .await
-            .expect("Failed to connect to Postgres instance.");
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to Postgres instance.");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
 
     // migrate database
-    let connection_pool = PgPool::connect(config.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres database.");
     sqlx::migrate!("./migrations")
